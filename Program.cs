511ee0b41ac0914079fa970a63a9faf5f0de6011
Petrik Diak <diak@petrik.hu>
2023-09-28 09:19:48 +0200

    Kiiratjuk a consolra az adatainkat

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SzogOOP;
 
 namespace SokszogOOP
 {
@@ -10,8 +11,12 @@ namespace SokszogOOP
 	{
 		static void Main(string[] args)
 		{
-
-
+			Haromszog haromszog = new Haromszog();
+			Haromszog haromszog2 = new Haromszog(3,4,5);
+			Teglalap teglalap = new Teglalap(4,5);
+			Console.WriteLine(haromszog);
+			Console.WriteLine(haromszog2);
+			Console.WriteLine(teglalap);
 		}
 	}
 }
